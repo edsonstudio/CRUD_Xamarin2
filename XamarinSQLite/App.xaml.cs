91d3214b06c453d264b452b3286f3430162c26d9
Edson <edsont8@hotmail.com>
2020-06-30 10:52:07 -0300

    primeiro commit

---
+++ XamarinSQLite/App.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinSQLite
+{
+    public partial class App : Application
+    {
+        static SQLiteHelper db;
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        public static SQLiteHelper SQLiteDb
+        {
+            get
+            {
+                if (db == null)
+                {
+                    db = new SQLiteHelper(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "XamarinSQLite.db3"));
+                }
+                return db;
+            }
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
